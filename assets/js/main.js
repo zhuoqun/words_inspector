752e3e52ee6250565c89f13feb452631b386c761
Jack Wang <zhuoqunwang@gmail.com>
2013-04-24 17:58:23 +0800

    Add stats js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,18 @@ $(document).ready(function () {
   var inputWrap = $('#inputWrap');
   var resultWrap = $('#resultWrap');
   var output = $('#output');
+  var redWordsList = $('#redWords ul');
+
   articleInput.focus();
 
   function countWords (text, words) {
-    var i, word, reg, count, statsArr = [], l = words.length;
+    var i, word, reg, count, match, statsArr = [], l = words.length;
 
     for (i=0; i< l; i += 1) {
       word = words[i];
       reg = new RegExp(word, 'g');
-      count = text.match(reg).length;
+      match = text.match(reg);
+      count = match ? match.length : 0;
       if (count > 0) {
         statsArr.push({
           word: word,
@@ -28,16 +31,27 @@ $(document).ready(function () {
     };
   }
 
+  function renderStats(stats, node) {
+  }
+
   $('#startCheck').click(function (e) {
     e.preventDefault();
     // TODO: check the input text
-    var text = articleInput.val(), result;
+    var text = articleInput.val(), result, i, tmp;
     var redWords = ['认真贯彻'];
 
     result = countWords(text, redWords);
 
-    // Beautify for display
     output.html('<pre>' + result.text + '</pre>');
+
+    if (result.stats.length > 0) {
+      for (i = 0; i< result.stats.length; i += 1) {
+        tmp = result.stats[i];
+        redWordsList.append('<li><span>' + tmp.word + '</span> x ' + tmp.count + '</li>');
+      }
+    } else {
+      redWordsList.append('<li>无</li>');
+    }
     
     inputWrap.hide();
     resultWrap.show();

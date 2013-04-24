3e7ca261679ded9e3aca43b391008ac4409a1798
Jack Wang <zhuoqunwang@gmail.com>
2013-04-24 11:30:54 +0800

    Add words count function

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,38 @@ $(document).ready(function () {
   var output = $('#output');
   articleInput.focus();
 
+  function countWords (text, words) {
+    var i, word, reg, count, statsArr = [], l = words.length;
+
+    for (i=0; i< l; i += 1) {
+      word = words[i];
+      reg = new RegExp(word, 'g');
+      count = text.match(reg).length;
+      if (count > 0) {
+        statsArr.push({
+          word: word,
+          count: count
+        });
+        text = text.replace(reg, '<span class="red">' + word + '</span>');
+      }
+    }
+
+    return {
+      stats: statsArr,
+      text: text
+    };
+  }
+
   $('#startCheck').click(function (e) {
     e.preventDefault();
     // TODO: check the input text
-    var text = articleInput.val();
+    var text = articleInput.val(), result;
+    var redWords = ['认真贯彻'];
+
+    result = countWords(text, redWords);
 
     // Beautify for display
-    output.html('<pre>' + text + '</pre>');
+    output.html('<pre>' + result.text + '</pre>');
     
     inputWrap.hide();
     resultWrap.show();

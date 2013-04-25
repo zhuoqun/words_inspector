f797c09bcdee90fcbd030a18489c6b5d7d02029d
Jack Wang <zhuoqunwang@gmail.com>
2013-04-25 14:00:02 +0800

    Add ugly words stats

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,23 @@ $(document).ready(function () {
   var resultWrap = $('#resultWrap');
   var output = $('#output');
   var redWordsList = $('#redWords ul');
+  var uglyWordsList = $('#uglyWords ul');
 
   articleInput.focus();
 
-  function countWords (text, words) {
-    var i, word, reg, count, match, statsArr = [], l = words.length;
+  function countWords (text) {
+    var i, word, reg, count, percent, match, redStatsArr = [], uglyStatsArr = [], l, total;
+    total = text.length;
 
+    // red words
+    l = RED_WORDS.length;
     for (i=0; i< l; i += 1) {
-      word = words[i];
+      word = RED_WORDS[i];
       reg = new RegExp(word, 'g');
       match = text.match(reg);
       count = match ? match.length : 0;
       if (count > 0) {
-        statsArr.push({
+        redStatsArr.push({
           word: word,
           count: count
         });
@@ -25,33 +29,56 @@ $(document).ready(function () {
       }
     }
 
+    // ugly words
+    l = UGLY_WORDS.length;
+    for (i=0; i< l; i += 1) {
+      word = UGLY_WORDS[i];
+      reg = new RegExp(word, 'g');
+      match = text.match(reg);
+      count = match ? match.length : 0;
+      percent = Math.round(count / total * 10000) / 100.00;
+      if (percent > 2) {
+        uglyStatsArr.push({
+          word: word,
+          percent: percent + "%"
+        });
+        text = text.replace(reg, '<span class="ugly">' + word + '</span>');
+      }
+    }
+
     return {
-      stats: statsArr,
+      redStats: redStatsArr,
+      uglyStats: uglyStatsArr,
       text: text
     };
   }
 
   function renderStats(stats, node) {
+    var i, tmp;
+
+    if (stats.length > 0) {
+      for (i = 0; i< stats.length; i += 1) {
+        tmp = stats[i];
+        if (tmp.count) {
+          node.append('<li><span>' + tmp.word + '</span> x ' + tmp.count + '</li>');
+        } else {
+          node.append('<li><span>' + tmp.word + '</span> > ' + tmp.percent + '</li>');
+        }
+      }
+    } else {
+      node.append('<li>无</li>');
+    }
   }
 
   $('#startCheck').click(function (e) {
     e.preventDefault();
-    // TODO: check the input text
     var text = articleInput.val(), result, i, tmp;
-    var redWords = ['认真贯彻'];
 
-    result = countWords(text, redWords);
+    result = countWords(text);
+    renderStats(result.redStats, redWordsList);
+    renderStats(result.uglyStats, uglyWordsList);
 
     output.html('<pre>' + result.text + '</pre>');
-
-    if (result.stats.length > 0) {
-      for (i = 0; i< result.stats.length; i += 1) {
-        tmp = result.stats[i];
-        redWordsList.append('<li><span>' + tmp.word + '</span> x ' + tmp.count + '</li>');
-      }
-    } else {
-      redWordsList.append('<li>无</li>');
-    }
     
     inputWrap.hide();
     resultWrap.show();
@@ -59,7 +86,11 @@ $(document).ready(function () {
 
   $('#reInput').click(function (e) {
     e.preventDefault();
-    // TODO: remove the result
+
+    output.html('');
+    redWordsList.html('');
+    uglyWordsList.html('');
+
     resultWrap.hide();
     inputWrap.show();
     articleInput.focus();
